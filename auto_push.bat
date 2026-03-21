a175b2b088e7c982d6b20abf5ca219e0edb5151e
Michael Wang <wangzhanguoemail@163.com>
2026-03-21 11:28:11 +0800

    鎵嬪姩鏇存柊浣撹偛鎽樿 2026/03/21 周六

--- auto_push.bat
+++ auto_push.bat
@@ -1,14 +1,15 @@
-:: 1. 进入文件夹
+@echo off
+:: 1. 进入你的文件夹
 cd /d "D:\Github DESK\my-sports-site"
 
-:: 2. 记下变动
+:: 2. 记下变动（因为你已经手动把新文件放进来了）
 git add .
 
-:: 3. 提交
-git commit -m "自动更新 %date%"
+:: 3. 提交变动
+git commit -m "手动更新体育摘要 %date%"
 
-:: 4. 推送
+:: 4. 推送至 GitHub
 git push origin main
 
-:: 5. 别关窗口，让我看看报错
-pause
+:: 5. 任务完成，自动关闭窗口
+exit
